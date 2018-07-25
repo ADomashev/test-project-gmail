dc798decfbd7f2c4d24904a16096e4b5d780db5e
Alex Domashevski <alekseidomashevsky@gmail.com>
2018-07-25 19:45:56 +0300

    new commit

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -7,7 +7,7 @@ import utils.Singleton;
 public class BasePage {
 
     protected WebDriver driver;
-
+// coment
     public BasePage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
